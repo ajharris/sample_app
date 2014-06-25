7c1db37c3b98dbdf69eafdfea411afe80c244bd8
ajharris <andrewharris@Andrews-MacBook.local>
2014-06-25 14:05:07 -0400

    Finish user edit, update, index, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ SampleApp::Application.routes.draw do
   match '/signin',    to: 'sessions#new',           via: 'get'
   match '/signout',   to: 'sessions#destroy',       via: 'delete' 
   match '/signup',    to: 'users#new',              via: 'get'
-  match '/signup',    to: 'users#new',              via: 'get' 
   match '/help',      to: 'static_pages#help',      via: 'get'
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
